ec07f597bb7bcdb769886126b700c2ee0c2dfbb8
Alexey Kadyrov <alexeikadyrov@gmail.com>
2013-02-23 19:42:46 +0000

    changed version info

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -6,11 +6,9 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Doca Labs Project")]
-[assembly: AssemblyProduct("Utilities")]
-[assembly: AssemblyCopyright("Copyright ©2011 Doca Labs Projects")]
-[assembly: AssemblyTrademark("Apache License, Version 2.0")]
+[assembly: AssemblyProduct("HTTP Client Library")]
+[assembly: AssemblyCopyright("Copyright ©2013 Alexey Kadyrov")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -28,7 +26,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: CLSCompliant(false)]
 [assembly: NeutralResourcesLanguageAttribute("en")]
