a8979c3288b324d30b70a5c42080db5ddcbf07a4
Alexey Kadyrov <alexeikadyrov@gmail.com>
2013-07-03 22:57:10 +0100

    testing property converters

--- src/DocaLabs.Http.Client/Binding/PropertyConverting/SimpleValueConverter.cs
+++ src/DocaLabs.Http.Client/Binding/PropertyConverting/SimpleValueConverter.cs
@@ -3,17 +3,32 @@ using DocaLabs.Http.Client.Utils;
 
 namespace DocaLabs.Http.Client.Binding.PropertyConverting
 {
+    /// <summary>
+    /// Converts simple values, like int, string, Guid, etc.
+    /// </summary>
     public class SimpleValueConverter : IValueConverter
     {
         readonly string _name;
         readonly string _format;
 
+        /// <summary>
+        /// Initializes an instance of the SimpleValueConverter class.
+        /// </summary>
+        /// <param name="name">Name which should be used as the key for the converted value.</param>
+        /// <param name="format">If the format is non empty then string.Format is used.</param>
         public SimpleValueConverter(string name, string format)
         {
             _name = name;
             _format = format;
         }
 
+        /// <summary>
+        /// Converts a value using specified name as the key in the return dictionary.
+        /// If the value is null then the return collection will be empty.
+        /// If the format passed into the constructor is not empty string then string.Format is used.
+        /// </summary>
+        /// <param name="value">Value which should be converted.</param>
+        /// <returns>One key-value pair.</returns>
         public NameValueCollection Convert(object value)
         {
             var values = new NameValueCollection();
---
+++ tests/DocaLabs.Http.Client.Tests/Binding/PropertyConverting/SimpleValueConverterTests.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections.Specialized;
+using DocaLabs.Http.Client.Binding.PropertyConverting;
+using Machine.Specifications;
+
+namespace DocaLabs.Http.Client.Tests.Binding.PropertyConverting
+{
+    [Subject(typeof(SimpleValueConverter))]
+    class when_simple_value_converter_is_used_on_null_instance
+    {
+        static IValueConverter converter;
+        static NameValueCollection result;
+
+        Establish context =
+            () => converter = new SimpleValueConverter("Value", null);
+
+        Because of =
+            () => result = converter.Convert(null);
+
+        private It should_return_empty_collection =
+            () => result.ShouldBeEmpty();
+    }
+
+    [Subject(typeof(SimpleValueConverter))]
+    class when_simple_value_converter_is_used_with_null_format
+    {
+        static IValueConverter converter;
+        static NameValueCollection result;
+
+        Establish context = 
+            () => converter = new SimpleValueConverter("Value", null);
+
+        Because of =
+            () => result = converter.Convert(42);
+
+        It should_be_able_to_get_the_key_using_specified_name =
+            () => result.AllKeys.ShouldContainOnly("Value");
+
+        It should_be_able_to_convert_value =
+            () => result.GetValues("Value").ShouldContainOnly("42");
+    }
+
+    [Subject(typeof(SimpleValueConverter))]
+    class when_simple_value_converter_is_used_with_empty_format
+    {
+        static IValueConverter converter;
+        static NameValueCollection result;
+
+        Establish context =
+            () => converter = new SimpleValueConverter("Value", "");
+
+        Because of =
+            () => result = converter.Convert(42);
+
+        It should_be_able_to_get_the_key_using_specified_name =
+            () => result.AllKeys.ShouldContainOnly("Value");
+
+        It should_be_able_to_convert_value =
+            () => result.GetValues("Value").ShouldContainOnly("42");
+    }
+
+    [Subject(typeof(SimpleValueConverter))]
+    class when_simple_value_converter_is_used_with_specified_format
+    {
+        static IValueConverter converter;
+        static NameValueCollection result;
+
+        Establish context =
+            () => converter = new SimpleValueConverter("Value", "{0:X}");
+
+        Because of =
+            () => result = converter.Convert(42);
+
+        It should_be_able_to_get_the_key_using_specified_name =
+            () => result.AllKeys.ShouldContainOnly("Value");
+
+        It should_be_able_to_convert_value =
+            () => result.GetValues("Value").ShouldContainOnly("2A");
+    }
+
+    [Subject(typeof(SimpleValueConverter))]
+    class when_simple_value_converter_is_used_with_null_name
+    {
+        // ReSharper disable AssignNullToNotNullAttribute
+        static IValueConverter converter;
+        static NameValueCollection result;
+
+        Establish context =
+            () => converter = new SimpleValueConverter(null, null);
+
+        Because of =
+            () => result = converter.Convert(42);
+
+        It should_be_able_to_get_the_key_using_specified_name =
+            () => result.AllKeys.ShouldContainOnly(new string[] { null });
+
+        It should_be_able_to_convert_value =
+            () => result.GetValues(null).ShouldContainOnly("42");
+        // ReSharper restore AssignNullToNotNullAttribute
+    }
+
+    [Subject(typeof(SimpleValueConverter))]
+    class when_simple_value_converter_is_used_with_empty_name
+    {
+        static IValueConverter converter;
+        static NameValueCollection result;
+
+        Establish context =
+            () => converter = new SimpleValueConverter("", null);
+
+        Because of =
+            () => result = converter.Convert(42);
+
+        It should_be_able_to_get_the_key_using_specified_name =
+            () => result.AllKeys.ShouldContainOnly("");
+
+        It should_be_able_to_convert_value =
+            () => result.GetValues("").ShouldContainOnly("42");
+    }
+}
