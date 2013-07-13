43099811d08b2e2ae22c44fe488222a66c47a47c
Alexey Kadyrov <alexeikadyrov@gmail.com>
2013-07-13 23:01:45 +0100

    added comments

--- src/DocaLabs.Http.Client/Binding/PropertyConverting/IValueConverter.cs
+++ src/DocaLabs.Http.Client/Binding/PropertyConverting/IValueConverter.cs
@@ -2,8 +2,16 @@
 
 namespace DocaLabs.Http.Client.Binding.PropertyConverting
 {
+    /// <summary>
+    /// Defines methods to convert a value to name-value pairs. It's mainly used by property converters.
+    /// </summary>
     public interface IValueConverter
     {
+        /// <summary>
+        /// Converts a value.
+        /// </summary>
+        /// <param name="value">Value to be converted.</param>
+        /// <returns>One key-value pairs where the key would normally be the property name.</returns>
         NameValueCollection Convert(object value);
     }
 }
