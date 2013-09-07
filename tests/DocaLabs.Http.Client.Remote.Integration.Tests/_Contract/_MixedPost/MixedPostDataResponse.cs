7aea8e1b593e22cbd7a209dfb7cf7b5b3bae21fa
Alexey Kadyrov <alexeikadyrov@gmail.com>
2013-09-07 11:05:39 +0100

    extended testing

--- tests/DocaLabs.Http.Client.Remote.Integration.Tests/_Contract/_MixedPost/MixedPostDataResponse.cs
+++ tests/DocaLabs.Http.Client.Remote.Integration.Tests/_Contract/_MixedPost/MixedPostDataResponse.cs
@@ -8,6 +8,6 @@ namespace DocaLabs.Http.Client.Remote.Integration.Tests._Contract._MixedPost
         public Dictionary<string, string> Args { get; set; }
         public string Origin { get; set; }
         public Dictionary<string, string> Headers { get; set; }
-        public InnerPostDataRequest Json { get; set; }
+        public MixedPostDataRequest Json { get; set; }
     }
 }
